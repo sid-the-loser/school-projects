27e974fce51a6f3e619c99cde8afd47d866e3e58
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-10-23 13:38:08 +0530

    Create armstrong_palindrome_perfect.py

---
+++ armstrong_palindrome_perfect.py
@@ -0,0 +1,36 @@
+x = int(input("Enter the number here: "))
+
+x = [x, str(x)]
+
+x_ = ""
+
+for i in range(len(x[1]), 0, -1):
+    x_ = x_ + x[1][i-1]
+
+if x_ == x[1]:
+    print("This number is a palindrome!")
+
+else:
+    print("This number is not a palindrome!")
+
+x__ = 0
+
+for i in x_:
+   x__ += int(i)**3
+
+if x__ == x[0]:
+    print("This number is an armstrong number!")
+else:
+    print("This number is not an armstrong number!")
+
+x_ = 0
+
+for i in range(x[0]):
+    if x[0] % (i+1) == 0:
+        x_ += i+1
+
+if x_//2 == x[0]:
+    print("This number is perfect!")
+
+else:
+    print("This number is not perfect!")
