2d928f54d17d314e47e4d0174e8e1b1f55a87d22
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-10-23 12:37:27 +0530

    Create greater_and_lessthan_2.py

---
+++ greater_and_lessthan_2.py
@@ -0,0 +1,11 @@
+x = float(input("Enter a number: "))
+y = float(input("Enter another number: "))
+
+if x == y:
+    print(f"{x} = {y}")
+
+elif x > y:
+    print(f"{x} > {y}")
+
+else:
+    print(f"{x} < {y}")
